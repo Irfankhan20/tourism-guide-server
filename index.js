be213464574825e4fd0e0a7a594957b314398d88
Irfankhan20 <sipathan96@gmail.com>
2025-01-14 11:15:59 +0600

    sever connection with mongodb

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+//=====================================================================
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w7smd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // packages related apis======================================
+    const packageCollection = client.db("uniqueTravel").collection("packages");
+
+    //get all packages
+    app.get("/packages", async (req, res) => {
+      const result = await packageCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+//=======================================================================
 
 app.get("/", (req, res) => {
   res.send("tourism project running ");
