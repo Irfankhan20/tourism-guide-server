80081512defde8680dd1e1e503f7fecd0b3c70f6
Irfankhan20 <sipathan96@gmail.com>
2025-01-15 12:37:06 +0600

    get packages data by 3 random

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,9 +27,48 @@ async function run() {
     // packages related apis======================================
     const packageCollection = client.db("uniqueTravel").collection("packages");
 
-    //get all packages
+    //get 3 random packages
     app.get("/packages", async (req, res) => {
-      const result = await packageCollection.find().toArray();
+      const result = await packageCollection
+        .aggregate([{ $sample: { size: 3 } }])
+        .toArray();
+      res.send(result);
+    });
+
+    //get package data by id
+    app.get("/package/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await packageCollection.findOne(query);
+      res.send(result);
+    });
+
+    // guides related apis======================================
+    const guideCollection = client.db("uniqueTravel").collection("tourGuides");
+
+    //get 6 random guides
+    app.get("/guides", async (req, res) => {
+      const result = await guideCollection
+        .aggregate([{ $sample: { size: 6 } }])
+        .toArray();
+      res.send(result);
+    });
+
+    //user related apis============================================
+    const userCollection = client.db("uniqueTravel").collection("users");
+
+    //user post
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: "user already exists",
+          insertedId: null,
+        });
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
